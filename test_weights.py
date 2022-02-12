3f73525dabee7c082819df6a421ab9a25cfcec2d
Callum <Callumr714@icloud.com>
2022-02-12 23:07:11 +0000

    Return Frame

--- test_weights.py
+++ test_weights.py
@@ -10,7 +10,7 @@ class GalaxyClassifier:
     def __init__(self, file_list, weights):
         self.file_list = file_list
         self.model = self.load_model(weights)
-        self.classes = self.mode.names
+        self.classes = self.model.names
 
     def load_model(self, weights):
         # check device
@@ -36,31 +36,33 @@ class GalaxyClassifier:
     def class_convert(self, x):
         return self.classes[int(x)]
 
-    def draw(self, results, frame, thresh):
+    def draw(self, results, frame):
         labels, cord = results
         n = len(labels)
         x_shape = frame.shape[1]
         y_shape = frame.shape[0]
         for i in range(n):
             row = cord[i]
-            if row[4] >= thresh:
+            if row[4] >= 0.2:
                 x1 = int(row[0]*x_shape)
                 y1 = int(row[1]*y_shape)
                 x2 = int(row[2]*x_shape)
                 y2 = int(row[3]*y_shape)
                 bgr = (0,255,0)
                 cv.rectangle(frame, (x1,y1), (x2,y2), bgr, 2)
-                cv.putText(frame, self.class_convert(labels[i]), (x1, y1, cv.FONT_HERSHEY_COMPLEX, 0.9))
+                cv.putText(frame, self.class_convert(labels[i]), (x1, y1), cv.FONT_HERSHEY_SIMPLEX, 0.9,(118, 185, 0))
+        return frame
     def __call__(self):
         img = self.get_img()
 
         while True:
             results = self.score(img)
             img = self.draw(results, img)
-            cv.imshow("Detected", img)
+            cv.imwrite("Detected.jpg", img)
 
             if cv2.waitKey(5) & 0xFF ==27:
                 break
 
 
-detector = GalaxyClassifier(file_list='1.jpg', weights='we.pt')
+detector = GalaxyClassifier(file_list='100090.jpg', weights='we.pt')
+detector()
