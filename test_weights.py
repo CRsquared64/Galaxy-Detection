21e2c4ef6f9850db606ea150f9458728b20cd887
Callum <Callumr714@icloud.com>
2022-02-12 21:57:08 +0000

    Draw, call, convert

--- test_weights.py
+++ test_weights.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 import numpy
 import time
@@ -20,13 +21,38 @@ class GalaxyClassifier:
 
         model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights, force_reload=True)
         return model
-    def score(self, frame, x):
+
+    def get_img(self):
+        return cv2.imread(self.file_list)
+    def score(self, frame):
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
-        return labels, cord, self.classes[int(x)]
+        return labels, cord
 
-    def draw(self, results, frame):
+    def class_convert(self, x):
+        return self.classes[int(x)]
+
+    def draw(self, results, frame, thresh):
         labels, cord = results
         n = len(labels)
+        x_shape = frame.shape[1]
+        y_shape = frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= thresh:
+                x1 = int(row[0]*x_shape)
+                y1 = int(row[1]*y_shape)
+                x2 = int(row[2]*x_shape)
+                y2 = int(row[3]*y_shape)
+                bgr = (0,255,0)
+                cv.rectangle(frame, (x1,y1), (x2,y2), bgr, 2)
+                cv.putText(frame, self.class_convert(labels[i]), (x1, y1, cv.FONT_HERSHEY_COMPLEX, 0.9))
+    def __call__(self):
+        img = self.get_img()
+
+        while True:
+            results = self.score(img)
+            img = self.draw(results, img)
+            cv.imshow("Detected", img)
