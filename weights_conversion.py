d2c173f39bff91cf6684397f9a2bd24a8662974b
Callum <Callumr714@icloud.com>
2022-02-19 23:32:45 +0000

    Yaassified it

--- weights_conversion.py
+++ weights_conversion.py
@@ -4,14 +4,33 @@ import torchvision
 import torchvision.models as models
 import sys
 
-onnx_model_path = "Weights/conversion.onnx"
 
-weights = 'yolov5s.pt'
-model = torch.hub.load('ultralytics/yolov5', 'custom',path=weights)
-model.to('cpu')
+class ConvertWeights:
 
-model.eval()
+    def __init__(self, weights, output):
+        self.model = self.load_model(weights)
+        self.output = output
+
+    def device(self):
+
+        if torch.cuda.is_available():
+            self.device = 'cuda'
+            print(f"Using GPU{torch.cuda.get_device_name(0)}")
+        else:
+            self.device = 'cpu'
+            print("Using CPU")
+
+    def load_model(self, weights):
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights)
+        return model
+
+    def __call__(self, *args, **kwargs):
+        self.model.to(self.device())
+        self.model.eval()
+        dummy_input = torch.randn(1, 3, 224, 224)
+        torch.onnx.export(self.model, dummy_input, self.output, verbose=True, opset_version=11)
 
 
-dummy_input = torch.randn(1, 3, 224, 224)
-torch.onnx.export(model, dummy_input, onnx_model_path, verbose=True, opset_version=11)
+if __name__ == '__main__':
+    convert = ConvertWeights(weights='we.pt', output='Weights/conversion.onnx')
+    convert()
